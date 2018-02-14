122ec6daa3e35aed710ad37b88ea5e1385cc8089
Roman <rom.konashenkov@yandex.ru>
2018-02-14 18:48:07 +0300

    Устраняет недочеты

--- gulpfile.js
+++ gulpfile.js
@@ -55,7 +55,7 @@ gulp.task("html", function() {
 });
 
 gulp.task("sprite", function() {
-  return gulp.src("source/img/*.svg")
+  return gulp.src("source/img/sprite-*.svg")
     .pipe(svgstore({
       inlineSvg: true
     }))
@@ -73,7 +73,7 @@ gulp.task("serve", function() {
   });
 
   gulp.watch("source/sass/**/*.{scss,sass}", ["style"]);
-  gulp.watch("source/*.html", ["html"]);
+  gulp.watch("source/*.html", ["html"]).on("change", server.reload);
 });
 
 gulp.task("copy", function() {
