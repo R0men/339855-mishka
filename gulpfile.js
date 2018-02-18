c1b08d9b11f5f2c9a8f8f461ae6d8a7f0601b84b
Roman <rom.konashenkov@yandex.ru>
2018-02-18 11:36:54 +0300

    Добавляет picturefill

--- gulpfile.js
+++ gulpfile.js
@@ -58,7 +58,7 @@ gulp.task("html", function() {
 });
 
 gulp.task("sprite", function() {
-  return gulp.src("source/img/*.svg")
+  return gulp.src("source/img/sprite-*.svg")
     .pipe(svgstore({
       inlineSvg: true
     }))
